041fda2d6a6155a321b9962f927f45e1cd380413
shreedev44 <shreedevchambayil@gmail.com>
2025-07-02 10:20:34 +0530

    ci: deployment process shifted to sh file

---
+++ server/scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Pull latest deployment from remote repository
+git pull origin main
+
+for MODULE in api-gateway authentication problem job notification; do
+    YAML="server/k8s/production/minikube/services/$MODULE/deployment.yaml"
+    if [ -f "$YAML" ]; then
+    echo "📦 Applying $YAML"
+    kubectl apply -f "$YAML"
+    else
+    echo "❌ No deployment.yaml for $MODULE at $YAML"
+    fi
+done
